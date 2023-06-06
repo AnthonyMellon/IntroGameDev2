3fb8e3868e75fd951a2bbbfa4624b9e449d9deee
Anthony <anthonymellon@yahoo.com>
2023-06-06 21:47:38 +1200

    Upgrading monsters

--- Assets/Scripts/PlaceMonster.cs
+++ Assets/Scripts/PlaceMonster.cs
@@ -12,12 +12,34 @@ public class PlaceMonster : MonoBehaviour
         return monster == null;
     }
 
+    private bool CanUpgradeMonster()
+    {
+        if(monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.GetNextLevel();
+            if(nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void OnMouseUp()
     {
         if(CanPlaceMonster())
-        {
+        {            
             monster = ((GameObject)Instantiate(monsterPrefab, transform.position, Quaternion.identity));
             
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            //TODO: Deduct gold
+        }
+        else if(CanUpgradeMonster())
+        {            
+            monster.GetComponent<MonsterData>().IncreaseLevel();
             AudioSource audioSource = gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(audioSource.clip);
 
