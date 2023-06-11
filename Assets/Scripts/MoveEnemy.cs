e5ea6728c985870b41bda260a40e8225f3b5efc2
Anthony <anthonymellon@yahoo.com>
2023-06-11 21:44:03 +1200

    Spawning test enemy the follows a path

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    [HideInInspector] 
+    public GameObject[] waypoints;
+    private int currentWaypoint = 0;
+    private float lastWaypointSwitchTime;
+    public float speed = 1.0f;
+
+    private void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    private void Update()
+    {
+        Vector3 startPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 endPosition = waypoints[currentWaypoint + 1].transform.position;
+
+        float pathLength = Vector3.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+        if(gameObject.transform.position.Equals(endPosition))
+        {
+            if (currentTimeOnPath < waypoints.Length - 2)
+            {
+                currentTimeOnPath++;
+                lastWaypointSwitchTime = Time.time;
+                //TODO: rotate into move direction 
+            }
+            else
+            {
+                Destroy(gameObject);
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+                //TODO: deduct health
+            }
+        }
+    }
+}
