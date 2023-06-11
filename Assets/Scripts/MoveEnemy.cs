532e717d798d0a0cbfe0c84ebfd82f2c431e6a87
Anthony <anthonymellon@yahoo.com>
2023-06-11 21:51:16 +1200

    Enemy rotates in direction it's moving

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -31,7 +31,7 @@ public class MoveEnemy : MonoBehaviour
             {
                 currentWaypoint++;
                 lastWaypointSwitchTime = Time.time;
-                //TODO: rotate into move direction 
+                RotateIntoMoveDirection();
             }
             else
             {
@@ -41,5 +41,19 @@ public class MoveEnemy : MonoBehaviour
                 //TODO: deduct health
             }
         }
+    }
+
+    private void RotateIntoMoveDirection()
+    {
+        Vector3 newStartPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 newEndPosition = waypoints[currentWaypoint + 1].transform.position;
+        Vector3 newDirection = (newEndPosition - newStartPosition);
+
+        float x = newDirection.x;
+        float y = newDirection.y;
+        float rotationAngle = Mathf.Atan2(y, x) * 180 / Mathf.PI;
+
+        GameObject sprite = gameObject.transform.Find("Sprite").gameObject;
+        sprite.transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
     }
 }
